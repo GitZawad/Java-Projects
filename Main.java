2639d61466b075b7f2dbdca92a802bac3846c2f5
Kazi Zawad Uddin <140585463+GitZawad@users.noreply.github.com>
2025-01-03 18:02:29 +0800

    Array Dequeue

--- Main.java
+++ Main.java
@@ -1,25 +1,17 @@
-interface parent{
-    void meth1();
-}
-
-interface parent2{
-    void meth2(int a);
-}
+import java.util.ArrayDeque;
 
 public class Main {
     public static void main(String[] args) {
-        parent p1 = new parent() {
-            @Override
-            public void meth1() {
-                System.out.println("This is meth1");
-            }
-        };
-        p1.meth1();
+        ArrayDeque q1 = new ArrayDeque();
+        q1.add(4);
+        q1.add(5);
+        q1.addFirst(9);
+        q1.addLast(80);
 
-        parent2 p2 = (a) -> {
-            System.out.println("This is meth "+a);
-        };
-        p2.meth2(2);
+        System.out.println(q1);
+        //System.out.println(q1.peek());
+        System.out.println(q1.pop());
+        System.out.println(q1);
+        System.out.println(q1.remove());
     }
-
 }
